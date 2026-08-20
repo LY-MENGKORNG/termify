6099046ea8a34a8993316f7b9e16b8e27ec7c209
LY-MENGKORNG <mengkorng.ly@student.passerellesnumeriques.org>
2026-08-21 01:03:14 +0700

    chore(workspace-structure): fix confusing files

--- crates/core/src/error/mod.rs
+++ crates/core/src/error/mod.rs
@@ -1,11 +1,11 @@
 pub mod config;
 pub mod logger;
 pub mod lyric;
-pub mod spotify;
+pub mod playback;
 pub mod theme;
 
 pub use config::*;
 pub use logger::*;
 pub use lyric::*;
-pub use spotify::*;
+pub use playback::*;
 pub use theme::*;
